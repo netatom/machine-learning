af32b5e0ba45cfc739b9d01b53ddb3522310649d
Ohsung Kwon <any7838@gmail.com>
2019-04-17 00:18:04 +0900

    pdo
    pdo

---
+++ pdo/index.php
@@ -0,0 +1,39 @@
+<?php
+	include 'head.html';
+?>
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Title</h1>
+      <p>Content</p>
+    </div>
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-32 w3-center"></p>
+      <p class="w3-border w3-padding-large w3-padding-64 w3-center"></p>
+    </div>
+  </div>
+
+  <!-- Pagination -->
+  <div class="w3-center w3-padding-32">
+    <div class="w3-bar">
+      <a class="w3-button w3-black" href="#">1</a>
+      <a class="w3-button w3-hover-black" href="#">2</a>
+      <a class="w3-button w3-hover-black" href="#">3</a>
+      <a class="w3-button w3-hover-black" href="#">4</a>
+      <a class="w3-button w3-hover-black" href="#">5</a>
+      <a class="w3-button w3-hover-black" href="#">&raquo;</a>
+    </div>
+  </div>
+
+  <footer id="myFooter">
+    <div class="w3-container w3-theme-l2 w3-padding-32">
+      <h4>Footer Menu</h4>
+    </div>
+
+    <div class="w3-container w3-theme-l1">
+      <p>Powered by 
+<a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+    </div>
+  </footer>
+<?php
+	include 'footer.html';
+?>
