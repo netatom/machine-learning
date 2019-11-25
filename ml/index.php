bc60de21f2e37a45376a48bf47fa5f1eb001c3ae
Ohsung Kwon <any7838@gmail.com>
2019-11-25 16:33:46 +0900

    20191125-3
    update

--- ml/index.php
+++ ml/index.php
@@ -7,7 +7,7 @@
 	  <p><a href="ml_knearestneighbors.php">Knearestneighbors</a></p>
 	  <p><a href="ml_svr.php">SVR</a></p>
 	  <p><a href="ml_knearestneighbors_chart.php">Knearestneighbors with chart</a></p>
-	  <p><a href="ml_knearestneighbors_chart2.php">Knearestneighbors with chart2</a></p>
+	  <p><a href="ml_knearestneighbors_chart_block.php">Knearestneighbors with chart2</a></p>
     </div>
   </div>
 
