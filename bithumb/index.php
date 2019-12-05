95cdc8e020e0795fa69d372e2487563fa5f28def
Ohsung Kwon <any7838@gmail.com>
2019-12-05 15:15:33 +0900

    Update index.php

--- bithumb/index.php
+++ bithumb/index.php
@@ -8,7 +8,6 @@
 	  <p><a href="public_orderbook.php">Public OrderBook</a></p>
 	  <p><a href="public_transaction_history.php">Public Transaction History</a></p>
 	  <p><a href="public_ticker_insert.php">Public Ticker Insert</a></p>
-	  <p><a href="public_ticker_insert_application.php">Public Ticker Application Insert</a></p>
     </div>
   </div>
 
