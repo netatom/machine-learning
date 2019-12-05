37f88b1a7ac86a6ed27c86f6d01b92bcf02620c1
Ohsung Kwon <any7838@gmail.com>
2019-12-05 16:47:17 +0900

    Update index.php

--- bithumb/index.php
+++ bithumb/index.php
@@ -4,6 +4,11 @@
 
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
+	  <p><a href="private_account.php">Private Account</a></p>
+	  <p><a href="private_balance.php">Private Balance</a></p>
+	  <p><a href="private_wallet_address.php">Private Wallet Address</a></p>
+	  <p><a href="private_ticker.php">Private Ticker</a></p> 
+	    
 	  <p><a href="public_ticker.php">Public Ticker</a></p>
 	  <p><a href="public_orderbook.php">Public OrderBook</a></p>
 	  <p><a href="public_transaction_history.php">Public Transaction History</a></p>
